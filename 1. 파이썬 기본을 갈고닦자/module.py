8f910b63a392a40bead3a2cee252e1a90472e1d3
gabang2 <offbeat1020@naver.com>
2023-03-15 21:44:51 +0000

    DocString

--- 1. 파이썬 기본을 갈고닦자/module.py
+++ 1. 파이썬 기본을 갈고닦자/module.py
@@ -1,9 +1,15 @@
 
-# https://blogattach.naver.com/d045cc7f6d3734e8c4254b7b4eacd1a30c58a7468e/20230310_126_blogfile/offbeat1020_1678410920771_44n7GW_txt/test.txt
+# https://blogattach.naver.com/d045cc7f6d3734e8c4254b7b4eaed0ae0a5aa0418e/20230310_126_blogfile/offbeat1020_1678410920771_44n7GW_txt/test.txt
+
 from urllib.request import urlopen
 import sys # python script실행 시 인자를 받기 위함
 
 def fetch_words(url):
+    """
+    url주소에서 파일을 가져와 단어 리스트를 반환합니다.
+    :param url: 불어올 url
+    :return:
+    """
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -13,12 +19,23 @@ def fetch_words(url):
     return story_words
 
 def print_items(items):
+    """
+    items를 print
+    :param items:
+    :return:
+    """
     for item in items:
         print(item)
 
 def main(url):
+    """
+    url을 받아 단어를 print함
+    :param url: url
+    :return:
+    """
     words = fetch_words(url)
     print_items(words)
 
 if __name__ == '__main__':
+    print(sys.argv)
     main(sys.argv[1])
