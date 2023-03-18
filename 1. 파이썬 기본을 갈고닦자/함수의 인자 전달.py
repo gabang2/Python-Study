eb8b5be612bbb5f6d4955a82dc3f98592d46d7cc
gabang2 <offbeat1020@naver.com>
2023-03-18 17:23:55 +0000

    함수의 인자(default, dictionary unpacking, list unpacking)

--- 1. 파이썬 기본을 갈고닦자/함수의 인자 전달.py
+++ 1. 파이썬 기본을 갈고닦자/함수의 인자 전달.py
@@ -1,8 +1,42 @@
-m = [1,5,7]
-r = [10, 15, 20]
+def abc(a, b, c):
+    print(c, b, a)
 
-def replace(s):
-    s = [25, 30, 35]
-    print("s = ", s)
+print("-------------1")
+abc(10, 20, 30)
+print("-------------2")
+abc(*[100, 200, 300])
 
-replace(r)
+def abcd(*args):
+    print(args[0], args[1], args[2])
+
+print("-------------3")
+abcd(*[1000, 2000, 3000, 4000])
+
+print("-------------4")
+def air_line1(departure, arrival, flighttime):
+    print("출발지는 : ", departure)
+    print("도착지는 : ", arrival)
+    print("비행 시간은 : ", flighttime)
+
+myflight = {"departure": "서울", "arrival": "LA", "flighttime":"10시간"}
+air_line1(**myflight)
+
+print("-------------5")
+def air_line2(**kwargs):
+    print("출발지는 : ", kwargs['departure'])
+    print("도착지는 : ", kwargs['arrival'])
+    print("비행 시간은 : ", kwargs['flighttime'])
+
+myflight = {"departure": "서울", "arrival": "LA", "flighttime":"10시간"}
+air_line2(**myflight)
+
+print("-------------6")
+def air_line3(departure, **kwargs):
+    print("출발지는 : ", departure)
+    print("도착지는 : ", kwargs['arrival'])
+    print("비행 시간은 : ", kwargs['flighttime'])
+
+myflight = {"departure": "서울", "arrival": "LA", "flighttime":"10시간"}
+air_line3(**myflight)
+
+
