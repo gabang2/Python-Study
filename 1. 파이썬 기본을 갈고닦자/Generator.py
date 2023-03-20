368dbd34866e266f13b99e2b445e4f6855e226e3
gabang2 <offbeat1020@naver.com>
2023-03-20 23:49:33 +0100

    iterator를 만드는 yield(generator)

---
+++ 1. 파이썬 기본을 갈고닦자/Generator.py
@@ -0,0 +1,22 @@
+def generator1():
+    print("yield 1 전")
+    yield 1
+    print("yield 2 전")
+    yield 2
+    print("yield 3 전")
+    yield 3
+
+def generator2():
+    a = [10, 20, 30]
+    for i in a:
+        print('yield {}'.format(i))
+        yield i
+
+def generator3():
+    a = [10, 20, 30]
+    yield from a
+
+gen1 = generator1()
+gen2 = generator2()
+gen3 = generator3()
+gen_list = list(gen3)
