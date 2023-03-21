f9996a546744ddc29a3ee065d98d853cbd4c957b
gabang2 <offbeat1020@naver.com>
2023-03-21 03:09:52 +0100

    unittest사용

--- 1. 파이썬 기본을 갈고닦자/close.py
+++ 1. 파이썬 기본을 갈고닦자/close.py
@@ -5,9 +5,7 @@ class OpenClose:
         print('작업을 시작합니다.')
     
     def do_something(self):
-        print('작업을 시작합니다...')
-        print('작업을 시작합니다...')
-        print('작업을 시작합니다...')
+        print('작업을 하는 중입니다.')
 
     def close(self):
         print("작업을 종료합니다.")
@@ -18,7 +16,7 @@ def doOpenClose1():
     d.do_something()
     d.close()
 
-def doOpenClose2():
+def doOpenClose():
     with closing(OpenClose()) as d:
         d.open()
         d.do_something()
